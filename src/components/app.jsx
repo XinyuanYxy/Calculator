f937fb340f11fc21baac1ce87252f9a4bb712895
XinyuanYxy <yxychris@outlook.com>
2023-02-17 23:44:59 -0500

    finish login, register components, and logic that only users who logged in can see the calculator

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,19 +6,66 @@ import Calculator from "./content/calculate";
 import NotFound from "./content/notFound";
 import Login from "./content/login";
 import Register from "./content/register";
-
+import $ from "jquery";
 class App extends Component {
-  state = {};
+  state = {
+    is_loggedIn: false,
+    username: "",
+  };
+
+  componentDidMount() {
+    $.ajax({
+      url: "http://127.0.0.1:8000/get_status/",
+      type: "get",
+      success: (res) => {
+        if (res.result === "loggedIn") {
+          this.setState({ is_loggedIn: true, username: res.username });
+        } else {
+          this.setState({ is_loggedIn: false });
+        }
+      },
+    });
+  }
   render() {
     return (
       <React.Fragment>
-        <Navbar />
+        <Navbar
+          is_loggedIn={this.state.is_loggedIn}
+          username={this.state.username}
+        />
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/" element={<Home />} />
-          <Route path="/calculator" element={<Calculator />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route
+            path="/calculator"
+            element={
+              this.state.is_loggedIn ? (
+                <Calculator />
+              ) : (
+                <Navigate replace to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              this.state.is_loggedIn ? (
+                <Navigate replace to="/calculator" />
+              ) : (
+                <Login />
+              )
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              this.state.is_loggedIn ? (
+                <Navigate replace to="/calculator" />
+              ) : (
+                <Register />
+              )
+            }
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </React.Fragment>
